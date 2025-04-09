8ab2e3648e11eede362e22f110ea6a08e9f38592
Chromax-design <chromax.design@gmail.com>
2025-04-09 20:34:09 +0100

    skeleton loader added

--- src/components/ImageGallery.jsx
+++ src/components/ImageGallery.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import LightGallery from 'lightgallery/react';
 import 'lightgallery/css/lightgallery.css';
 import 'lightgallery/css/lg-zoom.css';
@@ -6,18 +6,28 @@ import 'lightgallery/css/lg-thumbnail.css';
 import lgThumbnail from 'lightgallery/plugins/thumbnail';
 import lgZoom from 'lightgallery/plugins/zoom';
 
-const ImageGallery = ({imageEntries}) => {
+const ImageGallery = ({ imageEntries }) => {
   return (
     <section className='grid grid-cols-[repeat(auto-fit,_minmax(250px,_1fr))] gap-5'>
-        {imageEntries.map(([path, module], index) => (
-          <div className='h-80 overflow-hidden group hover:scale-105 duration-300 ease-in-out' key={index}>
+      {imageEntries.map(([path, module], index) => {
+        const [loaded, setLoaded] = useState(false);
+
+        return (
+          <div
+            className='h-80 overflow-hidden group hover:scale-105 duration-300 ease-in-out relative'
+            key={index}
+          >
+            {!loaded && (
+              <div className="absolute top-0 left-0 w-full h-full bg-gradient-to-r from-gray-300 via-gray-200 to-gray-300 z-10" />
+            )}
+
             <LightGallery
               speed={500}
               plugins={[lgThumbnail, lgZoom]}
               download={true}
               closable={true}
               mobileSettings={{
-                controls: true, // Enables controls on mobile
+                controls: true,
                 showCloseIcon: true,
                 download: true,
               }}
@@ -25,17 +35,20 @@ const ImageGallery = ({imageEntries}) => {
             >
               <a href={module.default}>
                 <img
-                  key={index}
                   src={module.default}
                   alt={`gallery-${index}`}
-                  className="w-full h-full min-h-80 rounded object-cover object-top saturate-0 group-hover:scale-105 duration-300 ease-in-out group-hover:cursor-pointer group-hover:saturate-100 brightness-80 group-active:saturate-100 group-active:scale-105"
+                  onLoad={() => setLoaded(true)}
+                  className={`w-full h-full min-h-80 rounded object-cover object-top transition-all duration-300 ease-in-out
+                  saturate-0 brightness-80 
+                  ${loaded ? 'group-hover:scale-105 group-hover:cursor-pointer group-hover:saturate-100 group-active:saturate-100 group-active:scale-105' : 'opacity-0'}`}
                   loading='lazy'
                 />
               </a>
             </LightGallery>
           </div>
-        ))}
-      </section>
+        );
+      })}
+    </section>
   )
 }
 
